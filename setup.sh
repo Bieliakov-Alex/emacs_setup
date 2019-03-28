a34fe739605a1dac5dcb6f72474a4458383dfead
Aleksss <bieliakov.alex@gmail.com>
2019-03-28 11:55:12 +0200

    First commit with initial install

---
+++ setup.sh
@@ -0,0 +1,22 @@
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt isntall clang
+sudo apt-get install -y cmake
+sudo apt install git
+sudo apt-get install -y libclang-dev
+sudo apt-chache search clang-format
+sudo apt-get install clang-format
+sudo apt-get install emacs
+
+cd ~
+git clone https://github.com/Bieliakov-Alex/emacs_config.git .emacs.d
+
+cd ~
+git clone --recursive https://github.com/Andersbakken/rtags.git
+cd rtags
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
