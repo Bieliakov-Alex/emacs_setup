28bd701569bd8612d504fc4ea14f46dbb8abb196
Aleksss <bieliakov.alex@gmail.com>
2019-04-01 15:14:13 +0300

    Attempt to fix apt-get

--- setup.sh
+++ setup.sh
@@ -2,13 +2,14 @@
 sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt-get upgrade
 sudo apt-get update
-sudo apt install clang
-sudo apt-get install -y cmake
-sudo apt install git
-sudo apt-get install -y libclang-dev
 sudo apt-chache search clang-format
-sudo apt-get install clang-format
-sudo apt-get install emacs26
+sudo apt install -y clang cmake git libclang-dev clang-format emacs26
+# sudo apt-get install -y cmake
+# sudo apt install git
+# sudo apt-get install -y libclang-dev
+# sudo apt-chache search clang-format
+# sudo apt-get install clang-format
+# sudo apt-get install emacs26
 
 cd ~
 git clone https://github.com/Bieliakov-Alex/emacs_config.git .emacs.d
