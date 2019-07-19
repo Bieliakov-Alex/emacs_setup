01b6c4effea1028d34c67176970ca3397834f184
Oleksandr Bieliakov <bieliakov.alex@gmail.com>
2019-07-19 13:19:55 +0300

    Add cpplint setup

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt-get upgrade
 sudo apt-get update
 sudo apt-chache search clang-format
-sudo apt install -y clang cmake git libclang-dev clang-format emacs26
+sudo apt install -y clang cmake git libclang-dev clang-format python-pip emacs26
 
 cd ~
 git clone https://github.com/Bieliakov-Alex/emacs_config.git .emacs.d
@@ -16,6 +16,9 @@ cd build
 cmake ..
 make
 sudo make install
+
+#installing cpplint
+pip install cpplint
 
 #setting up cpplint location
 sed "s/aleksss/$USER/" ~/.emacs.d/settings.org > ~/.emacs.d/settings.org
